07d844146248d1883931bca4cb14d09a0c43c020
Antony Hruschev <antony_hruschev@epam.com>
2022-04-03 23:56:45 +0300

    HomeTaskOne

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String...args) {
+
+		Person edit = new Person();
+
+        	edit.name = "Edit";
+			
+		System.out.println("Name of first person is: " + edit.name);
+
+
+
+
+
+
+	} 
+}
